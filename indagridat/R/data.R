7244757431ab3782342d5a881afd518606384412
simon <simon.moulds10@imperial.ac.uk>
2015-08-14 12:37:33 +0100

    commit 14-08-2015: first commit

---
+++ indagridat/R/data.R
@@ -0,0 +1,56 @@
+#' District level agricultural statistics for India
+#'
+#' Dataset containing various.. TODO
+#'
+#' @format A data.frame
+#'
+#' @examples
+#'
+#' data(indiastat)
+"indiastat"
+
+#' Aggregated district level agricultural statistics for India
+#'
+#' Dataset containing various.. TODO
+#'
+#' @format A data.frame
+#'
+#' @examples
+#'
+#' data(indiastat1950)
+"indiastat1950"
+
+#' ICRISAT...
+#'
+#' Dataset containing various.. TODO
+#'
+#' @format A data.frame
+#'
+#' @examples
+#'
+#' data(icrisat)
+"icrisat"
+
+#' Aggregated ICRISAT...
+#'
+#' Dataset containing various.. TODO
+#'
+#' @format A data.frame
+#'
+#' @examples
+#'
+#' data(icrisat1950)
+"icrisat1950"
+
+
+#' Indian district map circa 1956
+#'
+#' Map showing Indian district boundaries... TODO
+#'
+#' @format A SpatialPolygonsDataFrame object
+#'
+#' @examples
+#'
+#' data(district1950)
+"district1950"
+
